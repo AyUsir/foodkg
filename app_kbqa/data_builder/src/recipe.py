b2d83719732cb8a3969f456bf3f6ec18f12e5318
KangXin <kangxin@jueceshu.com.cn>
2024-03-15 16:51:33 +0800

    无tag生成json数据

--- app_kbqa/data_builder/src/recipe.py
+++ app_kbqa/data_builder/src/recipe.py
@@ -8,32 +8,19 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from config.data_config import *
 from utils.io_utils import *
 
-
-def fetch_all_tags(sparql):
-    query = '''
-    PREFIX recipe-kb: <http://idea.rpi.edu/heals/kb/>
-    SELECT DISTINCT ?tag {
-        ?r recipe-kb:tagged ?tag .
-    }'''
-
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    tags = [x['tag']['value'] for x in results['results']['bindings']]
-    return tags
-
-def fetch_all_dishes(sparql, tag):
+def fetch_all_dishes(sparql):
     query = '''
         PREFIX recipe-kb: <http://idea.rpi.edu/heals/kb/>
-        SELECT DISTINCT ?r ?name {{
-            ?r recipe-kb:tagged {} .
+        SELECT DISTINCT ?r ?name {
+            ?r a recipe-kb:recipe .
             ?r rdfs:label ?name .
-        }}'''.format(tag)
+        }'''
 
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     dishes = [(x['r']['value'], x['name']['value']) for x in results['results']['bindings']]
+    print(len(dishes))
     return dishes
 
 def fetch_all_ingredients(sparql, dish):
@@ -41,13 +28,19 @@ def fetch_all_ingredients(sparql, dish):
         SELECT ?in {{
             {} recipe-kb:uses ?ii .
             ?ii recipe-kb:ing_name ?in
+            OPTIONAL {{ ?ii recipe-kb:ing_quantity ?quantity . }}
+            OPTIONAL {{ ?ii recipe-kb:ing_unit ?unit . }}
         }}
     '''.format(dish)
 
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
-    ingredients = [x['in']['value'] for x in results['results']['bindings']]
+    ingredients = [{
+        'name': x['in']['value'],
+        'quantity': x['quantity']['value'] if 'quantity' in x else None,
+        'unit': x['unit']['value'] if 'unit' in x else None
+    } for x in results['results']['bindings']]
     return ingredients
 
 
@@ -60,23 +53,23 @@ if __name__ == '__main__':
     sparql = SPARQLWrapper(USE_ENDPOINT_URL)
 
     f_kg = open(os.path.join(opt['output'], 'recipe_kg.json'), 'w')
-    all_tags = fetch_all_tags(sparql)
-    for tag in all_tags:
-        tag_name = ' '.join(tag.split('/')[-1].split('%20'))
-        if len(tag_name) == 0:
-            continue
-        graph = {'uri': tag, 'name': [tag_name], 'alias': [], 'type': ['tag'], 'neighbors': {}}
-        dishes = fetch_all_dishes(sparql, '<{}>'.format(tag))
-        graph['neighbors']['tagged_dishes'] = []
-        for dish, dish_name in dishes:
-            dish_graph = {dish: {'name': [dish_name], 'uri': dish, 'alias': [], 'type': ['dish_recipe'], 'neighbors': {}}}
-            dish_graph[dish]['neighbors']['contains_ingredients'] = []
-            ingredients = fetch_all_ingredients(sparql, '<{}>'.format(dish))
-            for ingredient in ingredients:
-                ingredient_name = ' '.join(ingredient.split('/')[-1].split('%20'))
-                ingredient_graph = {ingredient: {'name': [ingredient_name], 'uri': ingredient, 'alias': [], 'type': ['ingredient']}}
-                dish_graph[dish]['neighbors']['contains_ingredients'].append(ingredient_graph)
-            graph['neighbors']['tagged_dishes'].append(dish_graph)
-        f_kg.write(json.dumps({tag: graph}) + '\n')
+    dishes = fetch_all_dishes(sparql)
+    i = 0
+    for dish, dish_name in dishes:
+        print(i+1)
+        i = i+1
+        dish_uri = dish 
+        dish_name = dish_name 
+        graph = {dish_uri: {'name': [dish_name], 'uri': dish_uri, 'alias': [], 'type': ['dish_recipe'], 'neighbors': {}}}
+        graph[dish_uri]['neighbors']['contains_ingredients'] = []
+        ingredients = fetch_all_ingredients(sparql, '<{}>'.format(dish_uri))
+        for ingredient in ingredients:
+            ingredient_name = ' '.join(ingredient['name'].split('/')[-1].split('%20'))
+            ingredient_quantity = ingredient['quantity']
+            ingredient_unit = ingredient['unit']
+            ingredient_graph = {ingredient['name']: {'name': [ingredient_name], 'uri': ingredient['name'], 'alias': [], 'type': ['ingredient'],'quantity':ingredient_quantity,'unit':ingredient_unit}}
+            graph[dish_uri]['neighbors']['contains_ingredients'].append(ingredient_graph)
+        f_kg.write(json.dumps({dish_uri: graph}) + '\n')
         f_kg.flush()
+    
     f_kg.close()
